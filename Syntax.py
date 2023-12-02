fb6872bbdc52d73728fff82a51426c368079721d
sumit0920 <sumitsharma.aset@gmail.com>
2023-12-02 21:04:14 +0530

    Adding new Vairable and Data Types File

---
+++ Syntax.py
@@ -0,0 +1,3 @@
+if 5>1:
+    print("5 is greater than 1")
+    print("5 is greater than 1")
---
+++ Variable and Data Type.py
@@ -0,0 +1,6 @@
+charahter_name = "John Smith"
+charahter_age = "35"
+print("There once was a man named " + charahter_name + " , ")
+print("he was " + charahter_age + " years old. ")
+print ("He really liked the name " + charahter_name + " , ")
+print ("but didn't like being " + charahter_age + " , ")
